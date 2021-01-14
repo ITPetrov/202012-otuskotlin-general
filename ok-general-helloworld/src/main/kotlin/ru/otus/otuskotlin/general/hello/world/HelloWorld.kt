641d6c19df9f786be3a01e35904185aa975b8a33
petrov <a.sav210@gmail.com>
2021-01-14 22:19:44 +0300

    my 2 commit from Idea

--- ok-general-helloworld/src/main/kotlin/ru/otus/otuskotlin/general/hello/world/HelloWorld.kt
+++ ok-general-helloworld/src/main/kotlin/ru/otus/otuskotlin/general/hello/world/HelloWorld.kt
@@ -1,5 +1,13 @@
 package ru.otus.otuskotlin.general.hello.world
 
+import java.lang.RuntimeException
+
 fun main() {
     println("Hello Big World!")
+
+    val x: Int? =null
+    //val y=x?.toDouble() ?: throw RuntimeException ("null is not allowed here!!!!")
+    if (x!=null) {
+        print("")
+    }
 }
