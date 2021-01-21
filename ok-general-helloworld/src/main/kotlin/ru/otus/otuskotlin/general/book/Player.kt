6a44ec8fd6f13d5860f4171a1762a8c875d36d13
petrov <a.sav210@gmail.com>
2021-01-21 23:58:25 +0300

    my 2 commit from Idea

--- ok-general-helloworld/src/main/kotlin/ru/otus/otuskotlin/general/book/Player.kt
+++ ok-general-helloworld/src/main/kotlin/ru/otus/otuskotlin/general/book/Player.kt
@@ -1,17 +1,21 @@
 package ru.otus.otuskotlin.general.book
 
 class Player (_name: String,
-              var hPoints:Int) {
-    var name=_name
-        get()=field.capitalize()
-        set(value){
-        field=value.trim()
-        }
-    constructor(name: String) :this (name,hPoints=100){
-        if (name=="Ivanov") hPoints=500
+              val health: Int) {
+    var race ="DWARF"
+    var town ="Bavaria"
+    val name =_name
+    val aligment: String
+    private var age=0
+
+    init {
+        println ("initializing player")
+        aligment ="GOOD"
+    }
+    constructor(_name: String) : this(_name,100){
+        town ="The shire"
     }
 }
-class Dice(){
-    val rolledValue
-        get()=(1..6).shuffled().first()
+fun main(){
+    Player("Madrid")
 }
