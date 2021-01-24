58639deb014405c3b0bffab81b49505ec3dd255e
petrov <a.sav210@gmail.com>
2021-01-24 17:23:26 +0300

    my 2 commit from Idea

--- ok-general-helloworld/src/main/kotlin/ru/otus/otuskotlin/general/hello/world/HelloWorld.kt
+++ ok-general-helloworld/src/main/kotlin/ru/otus/otuskotlin/general/hello/world/HelloWorld.kt
@@ -3,7 +3,7 @@ package ru.otus.otuskotlin.general.hello.world
 import java.lang.RuntimeException
 
 fun main() {
-    println("Hello Big World!!!")
+    println("Hello Big World!!!!")
 
     val v1:Int = "Some String".let{
         println ("string $it")
