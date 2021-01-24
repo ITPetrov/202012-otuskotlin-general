03a29c19c52644dd08b7619bd99f7cc3448f1429
petrov <a.sav210@gmail.com>
2021-01-24 21:21:11 +0300

    my 2 commit from Idea

---
+++ ok-general-dsl/src/test/kotlin/ru/otus/otuskotlin/general/dsl/DslTest.kt
@@ -0,0 +1,4 @@
+package ru.otus.otuskotlin.general.dsl
+internal class DslTest {
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
 rootProject.name = "otuskotlin-general"
 include("ok-general-helloworld")
+include("ok-general-dsl")
+include("ok-general-dsl")
+include("ok-general-dsl")
+include("ok-general-dsl")
